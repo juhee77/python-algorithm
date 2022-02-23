445822ea1eb62f919339b5abb1f6aa93433dc771
juhee77 <bora2233@naver.com>
2022-02-23 21:01:21 +0900

    add comment

--- 구현문제/문자열재정렬.py
+++ 구현문제/문자열재정렬.py
@@ -3,6 +3,12 @@
 instr = list(input())
 sum = 0
 ans = []
+'''
+이부분을 그냥 
+data = input()
+for i in data 해서 하나씩 읽어오는 방법도 있음
+'''
+
 for i in range(0,len(instr)):
     if instr[i].isdigit():
         sum += int(instr[i])     
